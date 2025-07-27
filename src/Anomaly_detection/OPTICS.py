91b1ae26f0f1ac98681e66956a87b73b8e048e7f
erikaspada <erika372000@gmail.com>
2025-07-28 00:59:00 +0200

    Update OPTICS.py

--- src/Anomaly_detection/OPTICS.py
+++ src/Anomaly_detection/OPTICS.py
@@ -2,34 +2,38 @@ from sklearn.cluster import OPTICS
 from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
-import pandas as pd
-import os
+from sklearn.preprocessing import StandardScaler
 
-optics = OPTICS(min_samples=3, xi=0.005, min_cluster_size=4)
-df['cluster_optics'] = optics.fit_predict(X_scaled)
-df['predicted_anomaly_optics'] = (df['cluster_optics'] == -1).astype(int)
+def detect_anomalies_op(df):
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(df.drop(columns=["Upgrade"]))
 
-# Visualization
-X_pca = PCA(n_components=2).fit_transform(X_scaled)
-plt.figure(figsize=(8, 6))
-plt.scatter(X_pca[:, 0], X_pca[:, 1], c=df['cluster_optics'], cmap='tab10', alpha=0.7)
-plt.xlabel('PCA 1')
-plt.ylabel('PCA 2')
-plt.title('Clustering con OPTICS')
-plt.colorbar(label="Cluster")
-plt.show()
+    optics = OPTICS(min_samples=3, xi=0.005, min_cluster_size=4)
+    df['cluster_optics'] = optics.fit_predict(X_scaled)
+    df['predicted_anomaly_optics'] = (df['cluster_optics'] == -1).astype(int)
 
-# Evaluation
-y_true = df['Upgrade']
-y_pred = df['predicted_anomaly_optics']
-precision = precision_score(y_true, y_pred)
-recall = recall_score(y_true, y_pred)
-f1 = f1_score(y_true, y_pred)
-conf_matrix = confusion_matrix(y_true, y_pred)
+    # Visualization
+    X_pca = PCA(n_components=2).fit_transform(X_scaled)
+    plt.figure(figsize=(8, 6))
+    plt.scatter(X_pca[:, 0], X_pca[:, 1], c=df['cluster_optics'], cmap='tab10', alpha=0.7)
+    plt.xlabel('PCA 1')
+    plt.ylabel('PCA 2')
+    plt.title('Clustering con OPTICS')
+    plt.colorbar(label="Cluster")
+    plt.show()
 
-print(f"OPTICS - Precision: {precision:.4f}")
-print(f"OPTICS - Recall: {recall:.4f}")
-print(f"OPTICS - F1-Score: {f1:.4f}")
-print("Matrice di Confusione:\n", conf_matrix)
+    # Evaluation
+    y_true = df['Upgrade']
+    y_pred = df['predicted_anomaly_optics']
+    precision = precision_score(y_true, y_pred)
+    recall = recall_score(y_true, y_pred)
+    f1 = f1_score(y_true, y_pred)
+    conf_matrix = confusion_matrix(y_true, y_pred)
+
+    print(f"OPTICS - Precision: {precision:.4f}")
+    print(f"OPTICS - Recall: {recall:.4f}")
+    print(f"OPTICS - F1-Score: {f1:.4f}")
+    print("Matrice di Confusione:\n", conf_matrix)
+
 
 
